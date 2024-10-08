1be4cf0e90eff2d7b1bce1306c0225daecdaca8d
Harshit Bisht <harshitbisht@Harshits-MacBook-Air.local>
2024-10-08 22:30:39 +0530

    updated terraform files

---
+++ terraform/ec2.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "github-actions-selfhosted-agent"
+
+  instance_type          = "t2.micro"
+  key_name               = "Agent1"
+  monitoring             = true
+  vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  subnet_ids             = module.vpc.private_subnets
+
+  tags = {
+    Terraform   = "true"
+    purpose = "github-actions-agent"
+  }
+}
